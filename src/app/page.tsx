e306544a7a7ec7506a1ed9fb418f9dd22089d036
LeandroFilie <leandro.gfilie@gmail.com>
2023-05-18 19:55:06 -0300

    feat: create no memories page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,54 @@
+import { User, Heart } from 'lucide-react';
+import Image from 'next/image';
+
+import nlwLogo from '@/assets/logo.svg';
+
 export default function Home() {
   return (
-    <>
-      <h1 className="font-sans">Hello World</h1>
-      <h1 className="font-alt">Teste</h1>
-    </>
+    <main className="grid grid-cols-2 min-h-screen">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 translate-x-1/2 rounded-full h-[288px] w-[526px] bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        <a href="aa" className="flex items-center gap-3 text-left transition-colors hover:text-gray-50">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="text-sm leading-snug max-w-[140px]">
+            <span className="underline">Crie sua conta</span>
+            {' '}
+            e salve suas memórias
+          </p>
+        </a>
+
+        <div className="space-y-5">
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div className="max-w-[420px] space-y-4">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">Sua cápsula do tempo</h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+            </p>
+          </div>
+
+          <a href="aa" className="inline-block uppercase rounded-full bg-green-500 px-5 py-3 font-alt text-sm leading-none text-black hover:bg-green-600">
+            Cadastrar Lembraça
+          </a>
+        </div>
+
+        <div className="text-sm leading-relaxed text-gray-200 flex items-start justify-normal gap-1">
+          Feito com <Heart className="h-5 w-5" /> por Leandro no NLW da Rocketseat
+        </div>
+      </div>
+
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança, comece a
+            {' '}
+            <a href="#a" className="underline hover:text-gray-50">criar agora!</a>
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
