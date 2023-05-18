5fdce386701ea821e97b92498f1c3aa2fe570c58
LeandroFilie <leandro.gfilie@gmail.com>
2023-05-18 18:27:53 -0300

    feat: added google fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1 className="font-sans">Hello World</h1>
+      <h1 className="font-alt">Teste</h1>
+    </>
   );
 }
