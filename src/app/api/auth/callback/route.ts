ad84856791fd025d004d0f893d30af1d6d70eb88
LeandroFilie <leandro.gfilie@gmail.com>
2023-05-21 18:47:03 -0300

    feat: added token in cookies

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,20 @@
+import { api } from '@/lib/api';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get('code');
+
+  const signinResponse = await api.post('/auth/signin', { code })
+
+  const { token } = signinResponse.data;
+
+  const redirectURL = new URL('/', request.url);
+  const cookiesEspiresInSeconds = 60 * 60 * 24 * 7; // 7 days
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookiesEspiresInSeconds}`,
+    }
+  });
+}
