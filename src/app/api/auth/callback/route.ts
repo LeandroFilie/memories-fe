580ef5fc31547a3c8aba4f6005ddd2f867fa9b84
LeandroFilie <leandro.gfilie@gmail.com>
2023-05-21 20:01:02 -0300

    feat: added middleware for access routes

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -4,12 +4,13 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get('code');
+  const redirectTo = request.cookies.get('redirectTo')?.value;
 
   const signinResponse = await api.post('/auth/signin', { code })
 
   const { token } = signinResponse.data;
 
-  const redirectURL = new URL('/', request.url);
+  const redirectURL = redirectTo ?? new URL('/', request.url);
   const cookiesEspiresInSeconds = 60 * 60 * 24 * 7; // 7 days
 
   return NextResponse.redirect(redirectURL, {
