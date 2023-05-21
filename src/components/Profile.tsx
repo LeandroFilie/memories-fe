fe34770929bdf86d9759caf69408a34ce93bf2a4
LeandroFilie <leandro.gfilie@gmail.com>
2023-05-21 19:30:20 -0300

    feat: added logout

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,7 @@
 import { getUser } from "@/lib/auth";
 import { User } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Profile() {
   const {name, avatarUrl} = getUser();
@@ -9,7 +10,7 @@ export default function Profile() {
       <Image src={avatarUrl} width={40} height={40} alt={name} className="h-10 w-10 rounded-full"></Image>
       <p className="text-sm leading-snug max-w-[140px]">
         {name}
-        <a href="#" className="block text-red-400 hover:text-red-300">Quero sair</a>
+        <a href="/api/auth/logout" className="block text-red-400 hover:text-red-300">Quero sair</a>
       </p>
     </div>
   )
